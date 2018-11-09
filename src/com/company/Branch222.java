697cf043302e2c42debdb075a5abcab7d612ccf1
lk <lkghost7@gmail.com>
2018-11-09 11:18:47 +0300

    Revert "тест комита3"
    This reverts commit 3811030

---
+++ src/com/company/Branch222.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Branch222 {
+    public static void main(String[] args) {
+        System.out.println("бранч 222");
+        System.out.println("бранч 222");
+        System.out.println("бранч 222");
+        System.out.println("бранч 222");
+        System.out.println("бранч 222");
+        System.out.println("бранч 222");
+    }
+}
