be6169425461a027ecd2528ca581952fdeae7761
lk <lkghost7@gmail.com>
2018-11-09 11:34:48 +0300

    мерж мастера

--- src/com/company/BranchMaster111.java
+++ src/com/company/BranchMaster111.java
@@ -6,6 +6,12 @@ public class BranchMaster111 {
         System.out.println("BranchMaster1111");
         System.out.println("BranchMaster1111");
         System.out.println("BranchMaster1111");
-        System.out.println("BranchMaster1111");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
+        System.out.println("BranchMaster3333");
     }
 }
