bc2d863cc128e47f63b24be11a9879159877d87b
lk <lkghost7@gmail.com>
2018-11-09 11:54:40 +0300

    Cherypik 222

---
+++ src/com/company/Cherypik2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Cherypik2 {
+    public static void main(String[] args) {
+        System.out.println("Cherypik2222");
+        System.out.println("Cherypik2222");
+        System.out.println("Cherypik2222");
+        System.out.println("Cherypik2222");
+        System.out.println("Cherypik2222");
+        System.out.println("Cherypik2222");
+    }
+}
