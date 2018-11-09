f8e58622f2a55cb0da07f5f15c8f30aec64e115c
lk <lkghost7@gmail.com>
2018-11-09 11:55:32 +0300

    Cherypik 3333

---
+++ src/com/company/Cherypik33.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Cherypik33 {
+    public static void main(String[] args) {
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+        System.out.println("Cherypik333");
+    }
+}
