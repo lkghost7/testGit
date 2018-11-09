f2e08f7b38a9dd02518071e06403be0242eaa6bc
lk <lkghost7@gmail.com>
2018-11-09 11:30:18 +0300

    222

---
+++ src/com/company/BranchMaster222.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class BranchMaster222 {
+    public static void main(String[] args) {
+        System.out.println("BranchMaster222");
+        System.out.println("BranchMaster222");
+        System.out.println("BranchMaster222");
+        System.out.println("BranchMaster222");
+        System.out.println("BranchMaster222");
+    }
+}
