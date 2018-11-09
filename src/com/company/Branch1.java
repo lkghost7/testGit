b607ef691a39c350ec1e4cf0f9f552140825f914
lk <lkghost7@gmail.com>
2018-11-09 10:48:45 +0300

    тест комита

--- src/com/company/Branch1.java
+++ src/com/company/Branch1.java
@@ -4,6 +4,12 @@ public class Branch1 {
     public static void main(String[] args) {
         System.out.println("бранч1");
         System.out.println("бранч1");
-        System.out.println("бранч1");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
+        System.out.println("бранч2");
     }
 }
