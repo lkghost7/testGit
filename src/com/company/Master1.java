9f54651f41b4c217ae5878ce58cc39d7d2b1467d
lk <lkghost7@gmail.com>
2018-11-09 11:26:26 +0300

    777

---
+++ src/com/company/Master1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Master1 {
+    public static void main(String[] args) {
+        System.out.println("master111");
+        System.out.println("master111");
+        System.out.println("master111");
+        System.out.println("master111");
+        System.out.println("master111");
+    }
+}
